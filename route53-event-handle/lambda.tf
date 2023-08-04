1861ace6385232d04f6e0f4cfe8d145693550ab4
Aleksei Goncharov <alexwolk01@gmail.com>
2023-08-04 17:06:23 +0300

    fmt

--- route53-event-handle/lambda.tf
+++ route53-event-handle/lambda.tf
@@ -1,16 +1,16 @@
-resource local_file "lambda" {
+resource "local_file" "lambda" {
   content = templatefile("main.py", {
-      LOG_GROUP  = var.log_group_name
-      LOG_STREAM = var.log_stream_name
-    })
+    LOG_GROUP  = var.log_group_name
+    LOG_STREAM = var.log_stream_name
+  })
   filename = "lambda.py"
 }
 
 
 module "lambda_function" {
   depends_on = [local_file.lambda]
-  source  = "terraform-aws-modules/lambda/aws"
-  version = "= 5.3.0"
+  source     = "terraform-aws-modules/lambda/aws"
+  version    = "= 5.3.0"
 
   function_name = "route53_events_handler"
   description   = "Handle route53 events and add additional field"
@@ -21,7 +21,8 @@ module "lambda_function" {
   attach_policies = true
   policies = [
     "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-  "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"]
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+  ]
   number_of_policies = 2
 
 }
